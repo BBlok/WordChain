dd83b59ec49af1501b8d528b387567552ac79e2a
BBlok <williambrachoblok@gmail.com>
2016-03-25 10:39:52 -0500

    Test Input Box

---
+++ Assets/Scripts/TestTextInput.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TestTextInput : MonoBehaviour {
+
+	private InputField inputTextbox;
+
+	// Use this for initialization
+	void Start () {
+		inputTextbox = GetComponent<InputField> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (inputTextbox.text);
+	
+	}
+}
