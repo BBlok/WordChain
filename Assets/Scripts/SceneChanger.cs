fb6a4565fd9c47144fcf4fdc9e0e16e7aa847ef4
egoftcp <egoftcp@tamu.edu>
2016-04-21 21:14:27 -0500

    Added Basic Menu and Other Scenes
    Added Basic Menu and Other Scenes;
    Word History not yet implemented;
    Gameplay still needs to be finalized/debugged

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void GoToGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void GoToWordHistory()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void GoToAbout()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
