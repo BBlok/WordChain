898a44122869f25797e321eca162e72ebe570200
BBlok <williambrachoblok@gmail.com>
2016-04-29 13:24:41 -0500

    camera script finalized

--- Assets/Scripts/cameraScript.cs
+++ Assets/Scripts/cameraScript.cs
@@ -3,18 +3,21 @@ using System.Collections;
 
 public class cameraScript : MonoBehaviour {
 
-	private int newCount = 0;
-	private int prevCount = 0;
-	private Vector3 startPoint;
-	private Vector3 endPoint;
-	private bool moving = false;
+	public int newCount = 0;
+	public int prevCount = 0;
+	public Vector3 startPoint;
+	public Vector3 endPoint;
+	public bool moving = false;
+	public float move = 3.0f;
+	public int period = 3;
+
+	public Rigidbody2D marker;
 
 
 	public float smooth = 1.5f;
-	public float moveBy = 3.0f;
-	public int frequency = 1;
 	// Use this for initialization
 	void Start () {
+
 	
 	}
 	
@@ -24,13 +27,22 @@ public class cameraScript : MonoBehaviour {
 		newCount = GameObject.FindGameObjectsWithTag("Tower").Length;
 
 
+			
+			//this.transform.position = endpoint;
 
-		if (newCount - prevCount == frequency) {
 
-			//if (moving == false) {
-				//startPoint = new Vector3 (this.transform.position.x, this.transform.position.y, this.transform.position.z);
-				//endPoint = new Vector3 (this.transform.position.x, this.transform.position.y + 3.0f, this.transform.position.z);
-			//}
+		if (newCount - prevCount == period) {
+
+
+			if (this.transform.position.y <= marker.transform.position.y) {
+				startPoint = new Vector3 (this.transform.position.x, this.transform.position.y, this.transform.position.z);
+				endPoint = new Vector3 (this.transform.position.x, this.transform.position.y + 3.0f, this.transform.position.z);
+				this.transform.position = Vector3.Lerp (startPoint, endPoint, smooth * Time.deltaTime);
+			} else {
+				prevCount = newCount;
+				marker.transform.position = new Vector3 (marker.transform.position.x, marker.transform.position.y + move, marker.transform.position.z);
+			}
+
 
 			//moving = true;
 
@@ -38,15 +50,14 @@ public class cameraScript : MonoBehaviour {
 			//	prevCount = newCount;
 			//	moving = false;
 			//}
-				
-				//this.transform.position = Vector3.Lerp (startPoint, endPoint, smooth * Time.deltaTime);
-			this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + moveBy, this.transform.position.z);
-			prevCount = newCount;
 
 
 
 
 				//prevCount = newCount;
 		}
+
+		if (moving == true)
+			this.transform.position = endPoint;
 	}
 }
